9c7c902010bff4a0f4054f3a00165e1dc9dd23b2
vlevillar <vles2345@gmail.com>
2022-12-13 02:01:21 -0300

    commit 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { LandingPage } from './components/LandingPage/LandingPage';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Henry Dogs</h1>
+        <Route exact path="/" component={LandingPage}/>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPageStyle.css"
+
+
+export const LandingPage = () => {
+	return (
+		<div className="landing">
+			<h1 className="landingTitle">Welcome to my Dogs individual proyect!</h1>
+			<h2 className="landingSubTitle">“Dogs are not our whole life, but they make our lives whole.”</h2>
+			<Link exact to="/home" style={{ textDecoration: "none" }}>
+				<div className="wrap">
+					<button className="button">Enter</button>
+				</div>
+			</Link>
+		</div>
+	);
+};
